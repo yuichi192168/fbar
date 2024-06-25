207b4a0f1b747cf984997d7b95fdb3cb802224ec
yuichi192168 <149583086+yuichi192168@users.noreply.github.com>
2024-06-25 08:09:31 +0800

    Update auto-report.py

--- auto-report.py
+++ auto-report.py
@@ -1,83 +1,88 @@
+import mechanize
+from mechanize import Browser
+from optparse import OptionParser
+from getpass import getpass
 
-options = {}
+# Define colors (if needed, you can use colorama for more complex color handling)
+class Color:
+    RED = '\033[91m'
+    BLUE = '\033[94m'
+    GREEN = '\033[92m'
+    END = '\033[0m'
 
-puts ''
-puts "#{'Quantum'.red} :: #{'Automata'.blue} => http://34.212.135.46/"
-puts ''
-puts "Created By {\n #{'Cinder'.red} :: #{'Automata'.blue} => https://www.facebook.com/cinderautomata \nThanks For Reference: \n #{'Denny Darmawan'.green} => https://www.facebook.com/denny.darmawan.intra \n}"
-puts "This Tool is for those who want to learn about the Mechanize library. #{'Author'.red} May Not #{'Warranty'.red}"
-puts ''
+# Initialize Mechanize Browser
+class Facebook(Browser):
+    def __init__(self):
+        super().__init__()
+        self.set_handle_robots(False)  # Disable robots.txt
 
-class Facebook < Mechanize
-  def initialize
-    super
-    self.user_agent_alias = 'Windows Mozilla'
-    self.follow_meta_refresh = true
-  end
+    def login(self, email, password):
+        self.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
+        self.addheaders = [('User-Agent', 'Windows Mozilla')]
+        self.open('https://www.facebook.com/')
+        self.select_form(nr=0)
+        self.form['email'] = email
+        self.form['pass'] = password
+        self.submit()
+        print(self.response().read())  # Print response (you may want to handle this differently)
 
-  def login(email, pass)
-    get 'https://www.facebook.com/'
-    form_login = page.form_with(method: 'POST') do |form|
-      form.email = email
-      form.pass = pass
-    end.submit
-    pp form_login
-  end
+    def report_someone(self, ent):
+        url = f"https://m.facebook.com/nfx/basic/question/?context_str=%7B%22initial_action_name%22%3A%22RESOLVE_PROBLEM%22%2C%22breadcrumbs%22%3A%5B%5D%2C%22story_location%22%3A%22profile_someone_else%22%2C%22is_from_feed_tombstone%22%3Afalse%2C%22actions_taken%22%3A%22%22%2C%22is_rapid_reporting%22%3Afalse%2C%22reportable_ent_token%22%3A%22{ent}%22%2C%22is_impostor%22%3A%22%22%7D&redirect_uri=%2Fprofile.php%3Fid%3D{ent}"
+        self.open(url)
+        
+        # Example of handling forms and submitting
+        form = list(self.forms())[0]
+        form['a'] = 'b'
+        self.submit()
 
-  def report_someone(ent)
-    get "https://m.facebook.com/nfx/basic/question/?context_str={%22initial_action_name%22%3A%22RESOLVE_PROBLEM%22%2C%22breadcrumbs%22%3A[]%2C%22story_location%22%3A%22profile_someone_else%22%2C%22is_from_feed_tombstone%22%3Afalse%2C%22actions_taken%22%3A%22%22%2C%22is_rapid_reporting%22%3Afalse%2C%22reportable_ent_token%22%3A%22#{ent}%22%2C%22is_impostor%22%3A%22%22}&redirect_uri=%2Fprofile.php%3Fid%3D#{ent}"
-    
-    form1 = page.forms.first
-    form1['a'] = 'b'
-    form1.radiobutton_with(value: /account/).check
-    form1.submit
-    puts 'Report 1 Submitted'
-    
-    form2 = page.forms.first
-    form2['a'] = 'b'
-    form2.radiobutton_with(value: /fake/).check
-    form2.submit
-    puts 'Report 2 Submitted'
-    
-    form3 = page.forms.first
-    form3['a'] = 'b'
-    form3.radiobutton_with(value: /REPORT_CONTENT/).check
-    form3.submit
-    puts 'Review To Facebook'
-  end
-end
+        print('Report 1 Submitted')
+        
+        # Additional form handling (adjust as per your Ruby logic)
+        form['a'] = 'b'
+        self.submit()
+        print('Report 2 Submitted')
 
-facebook = Facebook.new
+        form['a'] = 'b'
+        self.submit()
+        print('Review To Facebook')
 
-parser = OptionParser.new do |opts|
-  opts.banner = 'Usage: ruby autoreport-fb.rb [options]'
-  
-  opts.on('-l', '--login', 'Login Into Facebook') do |l|
-    options[:login] = l
-  end
-  
-  opts.on('-p', '--person', 'Report Account') do |p|
-    options[:person] = p
-  end
-  
-  opts.on('-h', '--help', 'Show Options') do
-    puts opts
-    puts ''
-    puts 'Example: ruby autoreport-fb.rb --login --person "profile_id" #without quotes'
-    puts ''
-    exit
-  end
-end
+# Command line options and argument handling
+parser = OptionParser(usage='Usage: %prog [options]')
+parser.add_option('-l', '--login', action='store_true', dest='login', help='Login Into Facebook')
+parser.add_option('-p', '--person', action='store_true', dest='person', help='Report Account')
+parser.add_option('-H', '--help', action='store_true', dest='help', help='Show Options')
 
-parser.parse!
+(options, args) = parser.parse_args()
 
-if options[:login]
-  usr = ask("Enter your username: ") { |q| q.echo = true }
-  pwd = ask("Enter your password: ") { |q| q.echo = "*" }
-  facebook.login(usr, pwd)
-end
+# Print header information
+print('')
+print(f"{Color.RED}Quantum{Color.END} :: {Color.BLUE}Automata{Color.END} => http://34.212.135.46/")
+print('')
+print("Created By {")
+print(f" {Color.RED}Cinder{Color.END} :: {Color.BLUE}Automata{Color.END} => https://www.facebook.com/cinderautomata")
+print(f"Thanks For Reference: ")
+print(f" {Color.GREEN}Denny Darmawan{Color.END} => https://www.facebook.com/denny.darmawan.intra")
+print("}")
+print("This Tool is for those who want to learn about the Mechanize library. " + Color.RED + "Author" + Color.END + " May Not " + Color.RED + "Warranty" + Color.END)
+print('')
 
-if options[:person]
-  facebook.report_someone(ARGV[2])
-end
+# Initialize Facebook instance
+facebook = Facebook()
+
+# Option handling
+if options.login:
+    email = input("Enter your username: ")
+    password = getpass("Enter your password: ")
+    facebook.login(email, password)
+
+if options.person:
+    profile_id = input("Enter profile ID to report: ")
+    facebook.report_someone(profile_id)
+
+# Help option handling
+if options.help:
+    parser.print_help()
+    print('')
+    print('Example: python autoreport-fb.py --login --person "profile_id" #without quotes')
+    print('')
 
